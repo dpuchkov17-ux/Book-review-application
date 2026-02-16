54cd47b0757d7cd3614eee7099835c331e4d04fc
Dmitry <dpuchkov@gmail.com>
2026-02-16 15:42:01 -0500

    Changes 5-6 saved

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -2,13 +2,27 @@ const express = require('express');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
+
 const public_users = express.Router();
 
+public_users.post("/register", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+  
+    if (!username || !password) {
+      return res.status(400).json({ message: "username and password required" });
+    }
+  
+    // isValid(username) в этом проекте обычно означает "пользователь уже существует"
+    if (isValid(username)) {
+      return res.status(409).json({ message: "User already exists!" });
+    }
+  
+    users.push({ username, password });
+    return res.status(200).json({ message: "User successfully registered. Now you can login" });
+  });
 
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
@@ -46,8 +60,13 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+const isbn = req.params.isbn;
+if (books[isbn]) {
+      return res.send(JSON.stringify(books[isbn].reviews, null, 4));
+    } else {
+      return res.status(404).json({ message: "Book not found" });
+    }
+  
+  });
 
 module.exports.general = public_users;
